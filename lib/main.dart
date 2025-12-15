abc7aed526237905e4010566911f9a054f64eade
RubaidSM09 <rubaids0911@gmail.com>
2025-12-15 17:31:54 +0600

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'package:get/get.dart';
+
+import 'app/routes/app_pages.dart';
+import 'common/app_colors.dart';
+
+void main() {
+  runApp(
+    ScreenUtilInit(
+      designSize: const Size(360, 800),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return GetMaterialApp(
+          title: "SCUBE Task App",
+          initialRoute: AppPages.INITIAL,
+          getPages: AppPages.routes,
+          debugShowCheckedModeBanner: false,
+          defaultTransition: Transition.rightToLeft,
+          transitionDuration: Duration(milliseconds: 300),
+          theme: ThemeData(
+            scaffoldBackgroundColor: AppColors.background,
+            appBarTheme: AppBarTheme(
+              backgroundColor: AppColors.background,
+              foregroundColor: AppColors.background,
+            ),
+          ),
+        );
+      },
+    ),
+  );
+}
